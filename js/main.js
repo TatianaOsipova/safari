a0c93d3ce486689b685e17e2b1cbfd72f5647271
Tatiana Osipova <otv120780@gmail.com>
2023-06-30 19:02:59 -0400

    feat: change path to images

--- js/main.js
+++ js/main.js
@@ -9,8 +9,8 @@ $(function(){
 	});
 
 	$('.gallery__slider').slick({
-		prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="../safari/images/arrow-left.svg" alt="arrow-left"></button>',
-		nextArrow: '<button type="button" class="slick-btn slick-next"><img src="../safari/images/arrow-right.svg" alt="arrow-right"></button>',
+		prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="images/arrow-left.svg" alt="arrow-left"></button>',
+		nextArrow: '<button type="button" class="slick-btn slick-next"><img src="images/arrow-right.svg" alt="arrow-right"></button>',
 	});
 
 	$('.gallery__item-inner').magnificPopup({
